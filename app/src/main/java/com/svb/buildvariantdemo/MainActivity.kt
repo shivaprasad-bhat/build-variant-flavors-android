cff6efac1ebeb5e8bf616ab26afb7d44b52c7a99
Shivaprasad Bhat <svbhat.neelmane@gmail.com>
2021-05-20 23:14:13 +0530

    Adding demo project code

---
+++ app/src/main/java/com/svb/buildvariantdemo/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.svb.buildvariantdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val clickable: Button = findViewById<Button>(R.id.clickable)
+
+        clickable.setOnClickListener {
+            myFun()
+        }
+    }
+
+    private fun myFun(){
+        Log.d("MyTag", "Inside Function")
+    }
+}
